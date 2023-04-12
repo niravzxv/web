b710736fdaa51f346937de5bc2cc14568bc6cc21
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2023-04-12 13:07:47 +0530

    "digital marketing"

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,8 @@
 function Navbar() {
-  const navbar = document.getElementById("Navbar");
-  const Container = document.createElement("div");
-  Container.classList.add("container");
-  Container.innerHTML = `
+	const navbar = document.getElementById("Navbar");
+	const Container = document.createElement("div");
+	Container.classList.add("container");
+	Container.innerHTML = `
   <link rel="canonical" href="https://shivayinfotech.org/" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="HandheldFriendly" content="true" />
@@ -454,6 +454,6 @@ function Navbar() {
 		</div>
 	</header>
     `;
-  navbar.appendChild(Container);
+	navbar.appendChild(Container);
 }
 Navbar();
